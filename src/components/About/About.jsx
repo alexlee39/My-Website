1b7cc4f1a302e6966ebe5753b24b8d209dd992d4
Alex Lee <alexanderlee014@gmail.com>
2024-12-09 13:32:55 -0800

    Changed About Section of Portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,8 +12,7 @@ const About = () => {
     <p className="hobbies"> 
         Besides from just coding, I like playing video games. My top 3 video games currently
         would be League of Legends, Stardew Valley, and Terraria. Some other hobbies including 
-        going to the gym as I've been going to the gym and doing bodybuilding for around 2 years
-        ever since I went to college and just stuck with it.
+        going to the gym and messing with Arduinos.
     </p>
     <br/>
     <div className="lang-tiers"> My Most Proficient Tech Stack</div>
