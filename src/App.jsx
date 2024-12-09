564f4b07e5bc8b6695243297088cc62c051007f1
Alex Lee <alexanderlee014@gmail.com>
2024-12-09 13:27:24 -0800

    Added finished components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from 'react-router-dom'
+import Profile from './components/Profile/Profile.jsx';
+import About from './components/About/About.jsx';
+import ExperienceList from './components/ExperienceList/ExperienceList.jsx';
+
+import './index.css';
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='grid-container'>
+      <Profile/>
+      <About/>
+      <ExperienceList/>
+
+    </div>
   )
 }
 
